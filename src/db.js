1f23ad1366aabd5cf15bfa947005fcf53e7da378
Aqua <mauro.valenzuelatobar@gmail.com>
2024-01-08 19:50:41 -0300

    Database connection change

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/merndb')
+        await mongoose.connect('mongodb+srv://maurovalenzuelatobar:FONuXSzp935fngSK@cluster0.otpdvqj.mongodb.net/')
         console.log("db is connected")
     } catch (error) {
         console.log(error)
